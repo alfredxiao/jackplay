1134fef880800e99f6363463a62118cf260ef877
Alfred Xiao <xiaoyf@gmail.com>
2016-07-04 20:07:22 +1000

    remove redundant jar files from building

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,2 @@
 javac -cp ../lib/javassist-3.20.0-GA.jar jackplay/*.java jackplay/web/*.java
-jar -cmf manifest.txt jackplay.jar jackplay/*.class jackplay/web/*.class jackplay/web/resources/*
-cp jackplay.jar ../lib
+jar -cmf manifest.txt ../lib/jackplay.jar jackplay/*.class jackplay/web/*.class jackplay/web/resources/*
