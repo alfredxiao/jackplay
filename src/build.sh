314c13b7c7c66d05495b83d7484a19b4d709a4c7
Alfred Xiao <xiaoyf@gmail.com>
2016-07-04 20:08:21 +1000

    add shebang

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 javac -cp ../lib/javassist-3.20.0-GA.jar jackplay/*.java jackplay/web/*.java
 jar -cmf manifest.txt ../lib/jackplay.jar jackplay/*.class jackplay/web/*.class jackplay/web/resources/*
