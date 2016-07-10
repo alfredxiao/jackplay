4ca57807db0c13274bcb860f626f2af478b16ef4
Alfred Xiao <xiaoyf@gmail.com>
2016-07-10 19:48:06 +1000

    move autosuggest into jackplay.js (don't know how to separate and export/import them :( )

--- src/jackplay/web/resources/js/jackplay.js
+++ src/jackplay/web/resources/js/jackplay.js
@@ -1,4 +1,135 @@
 //<!--input id='playGround' name='playGround' id='playGround' size="60" placeholder="E.g. com.example.RegistrationService" title='Format: className.methodName'/-->
+//import {App} from 'auto-class-lookup';
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'C#',
+    year: 2000
+  },
+  {
+    name: 'C++',
+    year: 1983
+  },
+  {
+    name: 'Clojure',
+    year: 2007
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  {
+    name: 'Go',
+    year: 2009
+  },
+  {
+    name: 'Haskell',
+    year: 1990
+  },
+  {
+    name: 'Java',
+    year: 1995
+  },
+  {
+    name: 'Javascript',
+    year: 1995
+  },
+  {
+    name: 'Perl',
+    year: 1987
+  },
+  {
+    name: 'PHP',
+    year: 1995
+  },
+  {
+    name: 'Python',
+    year: 1991
+  },
+  {
+    name: 'Ruby',
+    year: 1995
+  },
+  {
+    name: 'Scala',
+    year: 2003
+  }
+];
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp(escapedValue, 'i');
+
+  return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
+
+class AutoClassLookup extends React.Component { // eslint-disable-line no-undef
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: getSuggestions('')
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSuggestionsUpdateRequested = this.onSuggestionsUpdateRequested.bind(this);
+  }
+
+  onChange(event, { newValue }) {
+    this.setState({
+      value: newValue
+    });
+  }
+
+  onSuggestionsUpdateRequested({ value }) {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  }
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Type \'c\'',
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest suggestions={suggestions} // eslint-disable-line react/jsx-no-undef
+                   onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+                   getSuggestionValue={getSuggestionValue}
+                   renderSuggestion={renderSuggestion}
+                   inputProps={inputProps} />
+    );
+  }
+}
+
+
 var PlayPanel = React.createClass({
   submitMethodLogging: function() {
     $.ajax({
@@ -18,7 +149,7 @@ var PlayPanel = React.createClass({
         <tr>
           <td>
             <label htmlFor="playGround">Target to trace: </label>
-
+            <div><AutoClassLookup/></div>
           </td>
           <td><button onClick={this.submitMethodLogging}>Play</button></td>
           <td><button onClick={this.requestToClearLogHistory}>Clear</button></td>
