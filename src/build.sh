cd06fbf905d782406d93d98ddc03a8346fc07758
Alfred Xiao <xiaoyf@gmail.com>
2016-07-28 21:19:56 +1000

    change dir structure; added ant build

--- src/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-javac -cp ../lib/javassist-3.20.0-GA.jar jackplay/*.java jackplay/web/*.java jackplay/play/domain/*.java jackplay/play/performers/*.java jackplay/play/*.java
-jar -cmf manifest.txt ../lib/jackplay.jar jackplay/*.class jackplay/web/*.class jackplay/play/domain/*.class jackplay/play/performers/*.class jackplay/play/*.class jackplay/web/resources/*
