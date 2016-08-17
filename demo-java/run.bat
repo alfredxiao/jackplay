50e3620bab8ee27cb44dd00c4ff3362b570c4b34
Alfred Xiao <xiaoyf@gmail.com>
2016-08-17 22:58:05 +1000

    added run.bat for windows users

---
+++ demo-java/run.bat
@@ -0,0 +1,3 @@
+javac myapp\greeter\*.java myapp\*.java
+
+java -Xbootclasspath/a:..\jackplay\dist\jackplay-bootstrap-latest.jar -javaagent:..\jackplay\dist\jackplay-agent-latest.jar="logLevel=debug,blacklist=java.net:java.nio,defaultTrace=myapp.greeter.NiceGreeter.greet(java.lang.String):myapp.Demo.main(java.lang.String[])" -cp . myapp.Demo 3
