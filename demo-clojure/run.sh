ce937bf4683e80498cd67dab1f1958fd383a2d0d
Alfred Xiao <xiaoyf@gmail.com>
2016-08-29 07:03:34 +1000

    use a different port

--- demo-clojure/run.sh
+++ demo-clojure/run.sh
@@ -1,3 +1,3 @@
 lein uberjar
 
-java -Xbootclasspath/a:../jackplay/dist/jackplay-bootstrap-latest.jar -javaagent:../jackplay/dist/jackplay-agent-latest.jar  -cp ./target/myapp-0.1.0-SNAPSHOT-standalone.jar myapp.core "Alfred"
+java -Xbootclasspath/a:../jackplay/dist/jackplay-bootstrap-latest.jar -javaagent:../jackplay/dist/jackplay-agent-latest.jar=port=8182  -cp ./target/myapp-0.1.0-SNAPSHOT-standalone.jar myapp.core "Alfred"
