fe59cc2e524ad509819e87fd8e42b587e1b228ae
Alfred Xiao <alfred.xiao@qantashotels.com>
2019-08-20 23:32:42 +1000

    add reason why Main was created

--- jackplay/src/jackplay/bootstrap/Main.java
+++ jackplay/src/jackplay/bootstrap/Main.java
@@ -1,5 +1,6 @@
 package jackplay.bootstrap;
 
+// this Main is added to workaround nexus repo requirements while validating jars being published
 public class Main {
     public static void main(String[] args) {
     }
