44c20cd62455645d6cb672296203d6bcb76e7fe1
Alfred Xiao <alfred.xiao@qantashotels.com>
2020-04-21 22:17:24 +1000

    make demo run.sh work in jdk 11

--- demo-java/run.sh
+++ demo-java/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 javac myapp/greeter/*.java myapp/*.java
 
-java -Xbootclasspath/a:../jackplay/dist/jackplay-bootstrap-latest.jar -javaagent:../jackplay/dist/jackplay-agent-latest.jar="logLevel=debug,defaultTrace=myapp.greeter.NiceGreeter.greet(java.lang.String):myapp.Demo.main(java.lang.String[])" -cp . myapp.Demo 3
+# JDK8 requires -Xbootclasspath/a:../jackplay/dist/jackplay-bootstrap-latest.jar
+# JDK11 does not requires it
+java -javaagent:../jackplay/dist/jackplay-agent-latest.jar="logLevel=debug,defaultTrace=myapp.greeter.NiceGreeter.greet(java.lang.String):myapp.Demo.main(java.lang.String[])" -cp . myapp.Demo 3
